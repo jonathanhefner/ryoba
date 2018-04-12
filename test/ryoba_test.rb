fa70917b02f8d055017962b2e3e168762c7a1823
Jonathan Hefner <jonathan.hefner@gmail.com>
2018-04-12 16:10:53 -0500

    Remove default test

--- test/ryoba_test.rb
+++ test/ryoba_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 class RyobaTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Ryoba::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
