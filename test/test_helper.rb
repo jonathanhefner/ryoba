7ee9826e7776815cb29cdd24e1f753bf194a7625
Jonathan Hefner <jonathan.hefner@gmail.com>
2018-04-12 16:50:44 -0500

    Add #make_node test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,14 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "ryoba"
 
 require "minitest/autorun"
+
+
+class Minitest::Test
+
+  private
+
+  def make_node(xml)
+    Nokogiri::XML("<root>#{xml}</root>").at("root")
+  end
+
+end
