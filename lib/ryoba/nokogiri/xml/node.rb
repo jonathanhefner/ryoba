a4b17d0ebdd4b35d3ee34050328cdf0ee5949523
Jonathan Hefner <jonathan.hefner@gmail.com>
2018-04-13 16:02:56 -0500

    Add Nokogiri::XML::Node#uri

--- lib/ryoba/nokogiri/xml/node.rb
+++ lib/ryoba/nokogiri/xml/node.rb
@@ -1,3 +1,5 @@
+require "uri"
+
 class Nokogiri::XML::Node
 
   # Equivalent to +.text.strip+, but raises an error if the result is an
@@ -12,6 +14,26 @@ class Nokogiri::XML::Node
       raise Ryoba::Error.new("No text in:\n#{self.to_html}")
     end
     result
+  end
+
+  # Returns the URI for an HTML link ( +<a>+ ), image ( +<img>+ ), or
+  # form ( +<form>+ ).  If possible, a relative URI will be converted to
+  # an absolute URI, based on the Node document's +url+ attribute.
+  #
+  # @return [URI, nil]
+  def uri
+    url = case self.name
+      when "a"
+        self["href"]
+      when "img"
+        self["src"]
+      when "form"
+        self["action"]
+      end
+
+    if url
+      self.document.url ? URI.join(self.document.url, url) : URI(url)
+    end
   end
 
 end
--- test/nokogiri/xml/node_test.rb
+++ test/nokogiri/xml/node_test.rb
@@ -22,4 +22,52 @@ class NokogiriXmlNodeTest < Minitest::Test
     assert_raises(Ryoba::Error) { node.text! }
   end
 
+  def test_uri_all_relative
+    expected = URI("/foo")
+
+    make_nodes_with_uri(expected).each do |node|
+      assert_equal expected, node.uri
+    end
+  end
+
+  def test_uri_all_relative_to_absolute
+    base = "http://localhost"
+    relative = "/foo"
+    expected = URI.join(base, relative)
+
+    make_nodes_with_uri(relative, base).each do |node|
+      assert_equal expected, node.uri
+    end
+  end
+
+  def test_uri_all_absolute
+    base = "http://example.com"
+    expected = URI("http://localhost/foo")
+
+    make_nodes_with_uri(expected, base).each do |node|
+      assert_equal expected, node.uri
+    end
+  end
+
+  def test_uri_all_missing
+    node = make_node('<a/> <img/> <form/> <div/>')
+
+    node.children.each do |no_uri|
+      assert_nil no_uri.uri
+    end
+  end
+
+  private
+
+  def make_nodes_with_uri(uri, document_uri = nil)
+    xml = <<-XML
+      <a href="#{uri}" />
+      <img src="#{uri}" />
+      <form action="#{uri}" />
+    XML
+
+    Nokogiri::XML::Document.parse(xml, document_uri && document_uri.to_s).
+      children.reject(&:text?)
+  end
+
 end
